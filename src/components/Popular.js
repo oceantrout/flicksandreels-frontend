2fe9f9d22547eeb99206066ac250503d108bc1cf
Tengfei <mtf123ok@gmail.com>
2022-12-15 20:31:32 +0800

    updating buttons

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,6 +1,7 @@
 import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 import "./Popular.css";
 function Popular() {
   const [movieData, setMovieData] = useState([]);
