929907b8e527a13cd5ec9fccdc48f963fcee0f5e
Tengfei <mtf123ok@gmail.com>
2022-12-22 23:56:04 +0800

    admin updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,57 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useLocalStorage } from "@har4s/use-local-storage";
 
-function Form (props) {
-    const [usernameInput, setUsernameInput] = useState("");
-    const [descriptionInput, setDescriptionInput] = useState("");
-    const [reviewInput, setReviewInput] = useState("");
+function Form(props) {
+  const [usernameInput, setUsernameInput] = useState("");
+  const [descriptionInput, setDescriptionInput] = useState("");
+  const [reviewInput, setReviewInput] = useState("");
+  const [uid, setUid] = useLocalStorage("UID");
 
-    const handleUsernameInput = (event) => {
-        setUsernameInput(event.target.value)
-    }
+  const handleUsernameInput = (event) => {
+    setUsernameInput(event.target.value);
+  };
 
-    const handleDescriptionInput = (event) => {
-        setDescriptionInput(event.target.value)
-    }
+  const handleDescriptionInput = (event) => {
+    setDescriptionInput(event.target.value);
+  };
 
-    const handleReviewInput = (event) => {
-        setReviewInput(event.target.value)
-    }
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const val = {
-            username: usernameInput,
-            heading: descriptionInput,
-            content: reviewInput} // Getting the value from the state
-        console.log('STATE: formInput:', val)
-        props.onHandleSubmit(val)
-        setUsernameInput("");
-        setDescriptionInput("");
-        setReviewInput("");
-    }
+  const handleReviewInput = (event) => {
+    setReviewInput(event.target.value);
+  };
 
-    return (    
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Username</label>
-                <input 
-                    type="text"
-                    value={usernameInput}
-                    onChange={handleUsernameInput}/><br />
-                <label>Description</label>
-                <input 
-                    type="text"
-                    value={descriptionInput}
-                    onChange={handleDescriptionInput}/><br />
-                <label>Review</label>
-                <input 
-                    type="text"
-                    value={reviewInput}
-                    onChange={handleReviewInput}/><br />
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const val = {
+      username: usernameInput,
+      heading: descriptionInput,
+      content: reviewInput,
+    }; // Getting the value from the state
+    console.log("STATE: formInput:", val);
+    props.onHandleSubmit(val);
+    setUsernameInput("");
+    setDescriptionInput("");
+    setReviewInput("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Username --must have</label>
+        <input
+          type="text"
+          placehoder={uid}
+          value={usernameInput}
+          onChange={handleUsernameInput}
+        />
+        <br />
+        <label>Description</label>
+        <input
+          type="text"
+          value={descriptionInput}
+          onChange={handleDescriptionInput}
+        />
+        <br />
+        <label>Review</label>
+        <input type="text" value={reviewInput} onChange={handleReviewInput} />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
